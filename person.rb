ee6c8e89cb5617e4362393b08bc190aef0acf3a3
Jonas Svensson <svensson.jonas@gmail.com>
2017-06-01 17:13:27 +0200

    add random name, age and gender

--- person.rb
+++ person.rb
@@ -1,11 +1,32 @@
 class Person
+  @@womenName = ['Maria', 'Anna', 'Margareta', 'Elisabeth', 'Eva', 'Kristina', 'Birgitta', 'Karin', 'Elisabet',
+    'Marie', 'Ingrid', 'Christina', 'Linnéa', 'Sofia', 'Kerstin', 'Marianne', 'Lena', 'Helena', 'Emma',
+    'Johanna', 'Linnea', 'Inger', 'Sara', 'Cecilia', 'Elin']
+
+  @@manName = ['Erik', 'Lars', 'Karl', 'Anders', 'Johan', 'Per', 'Nils', 'Carl', 'Mikael', 'Jan', 'Hans',
+    'Lennart', 'Olof', 'Peter', 'Gunnar', 'Sven', 'Fredrik', 'Bengt', 'Bo', 'Daniel', 'Gustav', 'Åke', 'Göran',
+    'Alexander', 'Magnus']
+
+
+  @@lastName = ['Johansson', 'Andersson', 'Karlsson', 'Nilsson', 'Eriksson', 'Larsson', 'Olsson',
+    'Persson', 'Svensson', 'Gustafsson', 'Pettersson', 'Jonsson', 'Jansson', 'Hansson',
+    'Bengtsson', 'Jönsson', 'Carlsson', 'Petersson', 'Lindberg', 'Magnusson', 'Lindström',
+    'Gustavsson', 'Olofsson', 'Lindgren', 'Axelsson']
+
   attr_accessor :name, :gender, :age
 
-  def initialize (args)
-    @name=args[:name]
-    @gender=args[:gender]
-    @age=args[:age]
-  end
+  def initialize ()
+    @age = rand(100)
+
+    if rand(2)==0
+      @name = @@manName[rand(25)] + " " + @@lastName[rand(25)]
+      @gender = "man"
+    else
+      @name = @@womenName[rand(25)] + " " + @@lastName[rand(25)]
+      @gender = "women"
+    end
+
+    end
 
 def to_s()
   return @name + " is a " + @age.to_s + " year old " + @gender + "\n"
